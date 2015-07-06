9214674c895dc6ef026d3576a572a289ff59bd17
find-happiness <w564263331@gmail.com>
2015-07-06 23:20:31 +0800

    Revert "修改"
    This reverts commit 10ea7745d12dbcd3303a191765e66ba8c6baccdf.

--- Tutorial02/main.cpp
+++ Tutorial02/main.cpp
@@ -13,7 +13,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline,
 	// Create the system object.
 	System = new SystemClass;
 	if(!System)
+	{
 		return 0;
+	}
 
 	// Initialize and run the system object.
 	result = System->Initialize();
